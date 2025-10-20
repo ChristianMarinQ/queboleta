f0706a23ae88f7f3d79cafd0b5dc7876f46c7b35
ChristianMarinQ <christian.marinq@uqvirtual.edu.co>
2025-10-20 00:14:09 -0500

    add libraries folder for shared utilities

--- frontend/lib/axiosClient.ts
+++ frontend/lib/axiosClient.ts
@@ -1,34 +1,38 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export const axiosClient = axios.create({
   withCredentials: true,
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// TODO: fix some issues
-axiosClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.error(error);
-    if (error.response) {
-      if (error.response.status === 400) {
-        error.message = "Bad request: 400";
-      } else if (
-        error.response.status === 403 ||
-        error.response.status === 401
-      ) {
-        error.message = "Not authorized: 401, 403";
-      } else if (error.response.status === 500) {
-        error.message = "Server error: 500";
-      }
-    } else if (error.request) {
-      console.error("No se recibió respuesta del servidor:", error.request);
-    } else {
-      console.error("Error al configurar la solicitud:", error.message);
-    }
-    return Promise.reject(error);
+const apiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+
+  withCredentials: true, // para enviar cookies como tu token
+  headers: {
+    "Content-Type": "application/json",
   },
-);
+});
+
+export async function api<T = any>(
+  path: string,
+  options?: AxiosRequestConfig,
+): Promise<T> {
+  try {
+    const res = await apiClient({
+      url: path,
+      ...options,
+    });
+    return res.data; // axios ya devuelve el JSON en .data
+  } catch (error: any) {
+    if (error.response) {
+      console.error("api fetch error:", error);
+      throw new Error(
+        `${error.response.data.message || error.response.statusText}`,
+      );
+    }
+    throw new Error(error.message || "Unknown API error");
+  }
+}
